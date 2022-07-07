64cec9787440bec6155726069883709f020cb080
samuelvictorol <samuel.softdev@outlook.com>
2022-07-07 13:21:48 -0300

    Tela de login + Inicio logica login

--- app-angular/src/app/app.module.ts
+++ app-angular/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CastServiceService } from './cast-service.service';
 import { GerenciarComponent } from './gerenciar/gerenciar.component';
@@ -12,7 +11,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     GerenciarComponent,
     HomeComponent
   ],
--- app-angular/src/app/cast-service.service.ts
+++ app-angular/src/app/cast-service.service.ts
@@ -4,14 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CastServiceService {
+  private AdminLogado! : boolean
+
   constructor() { }
-  
 
   ngOnInit() {
-
+    this.AdminLogado = false
   }
 
+  setAdminLogado(setLogado:boolean) {
+    this.AdminLogado = setLogado
+  }
 
+  getAdminLogado() {
+    return this.AdminLogado
+  }
 
 }
 
